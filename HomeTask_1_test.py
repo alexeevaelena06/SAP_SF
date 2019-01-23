09bf740a6dd43a5f01266d716d22fa2356006123
ElenaAlekseeva <alexeevaelena06@gmail.com>
2019-01-23 16:26:44 +0300

    Разработана проверка сайта первой ссылки

--- HomeTask_1_test.py
+++ HomeTask_1_test.py
@@ -1,26 +1,40 @@
+import pytest
 from selenium.webdriver.chrome import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-driver = webdriver.WebDriver('chromedriver.exe')
-driver.get("http://google.ru/")
-elem = WebDriverWait(driver=driver, timeout=10).\
-    until(expected_conditions.
-          presence_of_element_located((By.XPATH, '//div[@aria-label="Голосовой поиск"]/../..//input')))
-elem.click()
-elem.send_keys('Selenide',Keys.ENTER)
-elem = WebDriverWait(driver=driver, timeout=20).\
-    until(expected_conditions.
-          presence_of_element_located((By.XPATH, '(//h3)[1]')))
+def test_search():
+    driver = webdriver.WebDriver('chromedriver.exe')
+    print(1)
+    driver.get("http://google.ru/")
+    element = WebDriverWait(driver=driver, timeout=60).\
+            until(expected_conditions.
+                presence_of_element_located((By.XPATH, '//div[@aria-label="Голосовой поиск"]/../..//input')))
+    element.click()
+    print(2)
+    element.send_keys('Selenide',Keys.ENTER)
+    element = get_element('(//h3)[1]/..', driver=driver)
+    str = element.get_attribute('href')
 
-str = lists1[0].getattribute("a href")
-print(str)
+    if str == 'https://ru.selenide.org/':
+        print('GOT IT!')
+    else:
+        pytest.fail(f'Link"{str}" not equal "https://ru.selenide.org/')
 
-"""image = WebDriverWait(driver=driver, timeout=10).\
-    until(expected_conditions.
-        presence_of_element_located((By.XPATH, '(//img)[8]')))
-"""
-driver.close()
+def tearDown(self):
+    self.driver.close()
+
+def get_element(xpath, driver) -> WebElement:
+    locator = (By.XPATH, xpath)
+    WebDriverWait(driver=driver, timeout=60) \
+        .until(expected_conditions.
+               presence_of_element_located(locator=locator))
+    element = driver.find_element_by_xpath(xpath=xpath)
+    return element
+
+
+
 
