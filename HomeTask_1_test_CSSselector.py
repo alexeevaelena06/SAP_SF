4fa99b9ab2686da8f5800dfcf4b97cc9f76188e6
ElenaAlekseeva <alexeevaelena06@gmail.com>
2019-01-30 09:17:35 +0300

    Добален метод ожидания элемента.

--- HomeTask_1_test_CSSselector.py
+++ HomeTask_1_test_CSSselector.py
@@ -4,36 +4,37 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+"""All element locators"""
+field_search = 'input.gLFyf'
+first_result = 'div.r>a'
+text_more = 'a.Cq34nf'
+pictures = '[id=lb]>div>a'
+first_picture_site = 'a.iKjWAf'
+first_result_after_back = 'div.r>a'
+
 
 """TEST"""
-
 def test_search():
     driver = webdriver.WebDriver('D:\PyCharmProjects\SAP_SF\chromedriver.exe')
     driver.implicitly_wait(30)
     driver.get("http://google.ru/")
-    element = WebDriverWait(driver, 20).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, 'input.gLFyf')))
+    element = wait_and_get_element(driver, field_search)
     element.click()
     element.send_keys('Selenide', Keys.ENTER)
-    element = WebDriverWait(driver, 20).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.r>a')))
+    element = wait_and_get_element(driver, first_result)
     checklink = element.get_attribute('href')
     assert "https://ru.selenide.org/" in checklink
     print(checklink + " first result is correct")
-    element = WebDriverWait(driver, 20).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, 'a.Cq34nf')))
+    element = wait_and_get_element(driver, text_more)
     element.click()
-    element = WebDriverWait(driver, 20).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, '[id=lb]>div>a')))
+    element = wait_and_get_element(driver, pictures)
     element.click()
-    element = WebDriverWait(driver, 20).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, '[id=rg_s]>div>a:nth-child(2)')))
+    element = wait_and_get_element(driver, first_picture_site)
     checkpicture = element.text.strip()
     assert "selenide.org" in checkpicture
     print("First picture is corresponded to Selenide website")
     driver.back()
-    element = WebDriverWait(driver, 20).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.r>a')))
+    element = wait_and_get_element(driver, first_result_after_back)
     checkfirstlinkagain = element.get_attribute('href')
     assert checkfirstlinkagain in checklink
 
@@ -42,9 +43,9 @@ def test_search():
 def tearDown():
     driver.close()
 
-
-
-
-
+def wait_and_get_element(driver, locator):
+    element = WebDriverWait(driver, 20).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, locator)))
+    return element
 
 
