5afb70420df4d40db468df43ddd4a6e549ebbbe5
ElenaAlekseeva <alexeevaelena06@gmail.com>
2019-01-31 08:58:29 +0300

    Вариант кода с использованием словаря.

--- HomeTask_1_test_CSSselector.py
+++ HomeTask_1_test_CSSselector.py
@@ -5,38 +5,48 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 """All element locators"""
-field_search = 'input.gLFyf'
-first_result = 'div.r>a'
-text_more = 'a.Cq34nf'
-pictures = '[id=lb]>div>a'
-first_picture_site = 'a.iKjWAf'
-first_result_after_back = 'div.r>a'
+# field_search = 'input.gLFyf'
+# first_result = 'div.r>a'
+# text_more = 'a.Cq34nf'
+# pictures = '[id=lb]>div>a'
+# first_picture_site = 'div.rg_bx>a:nth-child(2)'
+# first_result_after_back = 'div.r>a'
 
+dic = {
+    "field_search": 'input.gLFyf',
+    "first_result": 'div.r>a',
+    "text_more": 'a.Cq34nf',
+    "pictures": '[id=lb]>div>a',
+    "first_picture_site": '[id=rg_s]>div>a:nth-child(2)',
+    "first_result_after_back": 'div.r>a'
+    }
 
 """TEST"""
 def test_search():
-    driver = webdriver.WebDriver('D:\PyCharmProjects\SAP_SF\chromedriver.exe')
-    driver.implicitly_wait(30)
-    driver.get("http://google.ru/")
-    element = wait_and_get_element(driver, field_search)
-    element.click()
-    element.send_keys('Selenide', Keys.ENTER)
-    element = wait_and_get_element(driver, first_result)
-    checklink = element.get_attribute('href')
-    assert "https://ru.selenide.org/" in checklink
-    print(checklink + " first result is correct")
-    element = wait_and_get_element(driver, text_more)
-    element.click()
-    element = wait_and_get_element(driver, pictures)
-    element.click()
-    element = wait_and_get_element(driver, first_picture_site)
-    checkpicture = element.text.strip()
-    assert "selenide.org" in checkpicture
-    print("First picture is corresponded to Selenide website")
-    driver.back()
-    element = wait_and_get_element(driver, first_result_after_back)
-    checkfirstlinkagain = element.get_attribute('href')
-    assert checkfirstlinkagain in checklink
+
+        driver = webdriver.WebDriver('D:\PyCharmProjects\SAP_SF\chromedriver.exe')
+        driver.implicitly_wait(60)
+        driver.get("http://google.ru/")
+        element = wait_and_get_element(driver, "field_search")
+        element.click()
+        element.send_keys('Selenide', Keys.ENTER)
+        element = wait_and_get_element(driver, "first_result")
+        checklink = element.get_attribute('href')
+        assert "https://ru.selenide.org/" in checklink
+        print(checklink + " first result is correct")
+        element = wait_and_get_element(driver, "text_more")
+        element.click()
+        element = wait_and_get_element(driver, "pictures")
+        element.click()
+        element = wait_and_get_element(driver, "first_picture_site")
+        checkpicture = element.text.strip()
+        assert "selenide.org" in checkpicture
+        print("First picture is corresponded to Selenide website")
+        driver.back()
+        element = wait_and_get_element(driver, "first_result_after_back")
+        checkfirstlinkagain = element.get_attribute('href')
+        assert checkfirstlinkagain in checklink
+
 
 """MODULES"""
 
@@ -44,7 +54,7 @@ def tearDown():
     driver.close()
 
 def wait_and_get_element(driver, locator):
-    element = WebDriverWait(driver, 20).until(
+    element = WebDriverWait(driver, 60).until(
         EC.visibility_of_element_located((By.CSS_SELECTOR, locator)))
     return element
 
